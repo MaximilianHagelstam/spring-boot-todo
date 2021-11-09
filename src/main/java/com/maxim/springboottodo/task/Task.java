822cfa21d2df0662c33f46fd5cecf8d43e42021a
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-11-09 19:20:55 +0200

    Create Task class

---
+++ src/main/java/com/maxim/springboottodo/task/Task.java
@@ -0,0 +1,54 @@
+package com.maxim.springboottodo.task;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
