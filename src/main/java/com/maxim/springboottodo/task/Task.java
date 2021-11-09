3650320a8770891105dd462f40c2b7ea037ce2d8
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-11-09 21:11:51 +0200

    Connect to postgres

--- src/main/java/com/maxim/springboottodo/task/Task.java
+++ src/main/java/com/maxim/springboottodo/task/Task.java
@@ -1,6 +1,21 @@
 package com.maxim.springboottodo.task;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Task {
+
+    @Id
+    @SequenceGenerator(
+            name = "task_sequence",
+            sequenceName = "task_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "task_sequence"
+    )
     private Long id;
     private String title;
     private String description;
